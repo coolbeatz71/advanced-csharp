f4e9bc041dacefc84235e983a97222a6318c7a10
coolbeatz71 <sigmacool@gmail.com>
2025-02-24 10:45:28 +0200

    chore: add comments, use exception and nullable expression

--- Program.cs
+++ Program.cs
@@ -2,16 +2,24 @@
 
 class Program
 {
-    private string? LastName { get; set; } = "";
+    private string LastName { get; set; } = "";
 
-    private string? FirstName { get; set; } = "";
+    private string FirstName { get; set; } = "";
 
-    private string PrintHelloFullName(string firstName, string lastName)
+    /// <summary>
+    /// This prints "Hello, firstName lastName" to the console
+    /// </summary>
+    /// <param name="firstName">firstName</param>
+    /// <param name="lastName">lastName</param>
+    /// <returns>Hello, firstName lastName</returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    private string PrintHelloFullName(string? firstName, string? lastName)
     {
-        this.FirstName = firstName;
-        this.LastName = lastName;
+        this.FirstName = firstName ?? throw  new ArgumentNullException(nameof(firstName));
+        this.LastName = lastName ?? throw  new ArgumentNullException(nameof(lastName));
         
-        return $"Hello, {this.FirstName} {this.LastName}!";
+        string fullName = String.Concat(firstName, " ", lastName);
+        return $"Hello, {fullName}!";
     }
 
     static void Main(string[] args)
@@ -23,6 +31,6 @@ class Program
         Console.WriteLine("What's your last name?");
         string? lastName = Console.ReadLine();
         
-        Console.WriteLine(pg.PrintHelloFullName(firstName!, lastName!));
+        Console.WriteLine(pg.PrintHelloFullName(firstName, lastName));
     }
 }

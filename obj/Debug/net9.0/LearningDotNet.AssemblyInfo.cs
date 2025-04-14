d1ab819164d92fa24c007142165c4438d22dc951
coolbeatz71 <sigmacool@gmail.com>
2025-04-14 18:51:55 +0200

    refactor: use the created program

--- obj/Debug/net9.0/LearningDotNet.AssemblyInfo.cs
+++ obj/Debug/net9.0/LearningDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84536396b85fb4fea4a78f5b800acb4ed41a608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a015ba366e72c7f24a597225049500782398e394")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
